711d577a99a7ea354996efe7e77316a68c3aeb41
Tomas Fiers <tomas.fiers@gmail.com>
2018-09-18 00:03:22 +0200

    Proper packaging

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="raincloud",
+    url="https://github.com/tfiers/raincloud",
+    packages=find_packages(),
+    install_requires=("seaborn"),
+)
